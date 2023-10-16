397e8e69526a036e01cf2a97dfefa1d8dc6f0a4b
Bruno Tenório <tenoriobn@gmail.com>
2023-10-16 12:25:22 -0300

    Adicionando métodos

--- Admin.js
+++ Admin.js
@@ -4,8 +4,12 @@ class Admin extends User {
   constructor(nome, email, nascimento, role = 'admin' , ativo = true) {
     super(nome, email, nascimento, role, ativo)
   }
+
+  criarCurso(nomeDoCurso, vagas) {
+    return `Curso de ${nomeDoCurso} criando ${vagas} vagas`
+  }
 }
 
 const novoAdmin = new Admin('Rodrigo', 'r@r.com', '2021-01-01');
 console.log(novoAdmin);
-console.log(novoAdmin.exibirInfos());
+console.log(novoAdmin.criarCurso('JS', 20));
